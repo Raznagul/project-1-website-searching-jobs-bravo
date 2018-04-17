65d106e05d8de7062d21041fd88e28b81944b3ef
DALOLIO\Andres <amda95@hotmail.com>
2018-04-16 22:57:22 -0600

    Added Landing and styles

--- searching-jobs-bravo/src/components/LandingPage.js
+++ searching-jobs-bravo/src/components/LandingPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Header from './HeaderMenu';
+import ContentLandPage from './ContentLandPage';
+import Footer from './Footer';
 
 class LandingPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div className="App">
+        <Header />
+        <ContentLandPage />
+        <Footer />
       </div>
     );
   }
