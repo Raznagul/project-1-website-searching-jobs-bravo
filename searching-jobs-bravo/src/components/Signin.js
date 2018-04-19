250e7c33f0d7c3cfbf4bedfa2e14e3687143bd9f
mimen093 <mimen093@gmail.com>
2018-04-19 17:46:11 -0600

    se ajusta los tamanios del formulario de sign in

--- searching-jobs-bravo/src/components/Signin.js
+++ searching-jobs-bravo/src/components/Signin.js
@@ -2,17 +2,14 @@ import React, { Component } from "react";
 import { withRouter, Link } from "react-router-dom";
 
 import { SignUpLink } from "./Signup";
-import { ForgettenPasswordLink} from "./ForgottenPassword";
+import { ForgettenPasswordLink } from "./ForgottenPassword";
 
 import { auth } from "../firebase";
 import * as routes from "../constants/routes";
 
 const SignInPage = ({ history }) => (
   <div>
-    <h1>SignIn</h1>
     <SignInForm history={history} />
-    <ForgettenPasswordLink />
-    <SignUpLink />
   </div>
 );
 
@@ -56,51 +53,62 @@ class SignInForm extends Component {
     const isInvalid = password === "" || email === "";
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email address</label>
-          <input
-            value={email}
-            onChange={event =>
-              this.setState(byPropKey("email", event.target.value))
-            }
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
+      <div className="row d-flex justify-content-center">
+        <div className="col-4">
+          <h1>Sign In</h1>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input
+                value={email}
+                onChange={event =>
+                  this.setState(byPropKey("email", event.target.value))
+                }
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                value={password}
+                onChange={event =>
+                  this.setState(byPropKey("password", event.target.value))
+                }
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+            {error && <p>{error.message}</p>}
+          </form>
+          <ForgettenPasswordLink />
+          <SignUpLink />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            value={password}
-            onChange={event =>
-              this.setState(byPropKey("password", event.target.value))
-            }
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-          />
-        </div>
-        <button disabled={isInvalid} type="submit" className="btn btn-primary">
-          Submit
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
+      </div>
     );
   }
 }
 
 const SignInLink = () => (
-    <p>
-      Already have an account? <Link to={routes.signin}>Sign In</Link>
-    </p>
-  );
+  <p>
+    Already have an account? <Link to={routes.signin}>Sign In</Link>
+  </p>
+);
 
 export default withRouter(SignInPage);
 
