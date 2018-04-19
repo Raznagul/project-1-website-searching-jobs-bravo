926136920f66edf91b355e77c22af5101ee2819c
DALOLIO\Andres <amda95@hotmail.com>
2018-04-18 23:53:17 -0600

    landing page content v1

--- searching-jobs-bravo/src/components/ContentLandPage.js
+++ searching-jobs-bravo/src/components/ContentLandPage.js
@@ -5,31 +5,29 @@ class ContentLandPage extends Component {
     render() {
         return (
             <div className="landPageContent">
-                <div className="container vertical-center">
-                    <section className="jumbotron text-center">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-12">
-                                    <h1 className="jumbotron-heading">Let experts do the searching</h1>
-                                </div>
-                            </div>
-                            <div className="row no-gutters pt-2">
-                                <div className="col-6 pr-1">
-                                    <div className="form-group">
-                                        <input type="text" className="form-control" id="palabraClave" placeholder="Palabra clave" />
-                                    </div>
-                                </div>
-                                <div className="col-5">
-                                    <div className="form-group">
-                                        <input type="text" className="form-control" id="ubicacion" placeholder="Ubicación" />
-                                    </div>
-                                </div>
-                                <div className="col-1">
-                                    <img src={require('./../images/analysis32.png')} className="icon" alt="Buscar" />
-                                </div>
+                <div className="jumbotron">
+                    <form className="formSearch" id="formSearch">
+                        <div className="row">
+                            <div className="col-12 text-center mb-2">
+                                <h1 className="jumbotron-heading">Let experts do the searching</h1>
                             </div>
                         </div>
-                    </section>
+                        <div className="row">
+                            <div className="col-sm-12 col-md-6 col-lg-6">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" id="palabraClave" placeholder="Palabra clave" />
+                                </div>
+                            </div>
+                            <div className="col-sm-12 col-md-5 col-lg-5">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" id="ubicacion" placeholder="Ubicación" />
+                                </div>
+                            </div>
+                            <div className="col-sm-12 col-md-1 col-lg-1 text-xs-right">
+                                <img src={require('./../images/analysis32.png')} className="icon" alt="Buscar" />
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         );
