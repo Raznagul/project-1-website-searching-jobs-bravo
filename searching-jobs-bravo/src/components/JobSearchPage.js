76e78cd40815ccfe6d4b213a610cecbaebf8ae60
Raznagul <dachdavid@gmail.com>
2018-04-21 14:57:24 -0600

    adding place holder for get json request

--- searching-jobs-bravo/src/components/JobSearchPage.js
+++ searching-jobs-bravo/src/components/JobSearchPage.js
@@ -74,7 +74,97 @@ class JobSearchPage extends Component {
         );
     }
 }
+/*
+class ItemListerasd extends Component {
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
 
-export default JobSearchPage;
+    componentDidMount() {
+        fetch('https://jobs.github.com/positions.json?description=python&location=new+york')
+            .then(result => {
+                this.setState({ items: result.json() });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <div>Items:</div>
+                {this.state.items.map(item => { return <div>{item.name}</div> })}
+            </div>
+        );
+    }
+}
+*/
+class ItemLister extends Component {
+    constructor() {
+        super();
+        this.state = { items: [] };
+        console.log(this.state);
+
+    }
+
+    componentDidMount() {
+        //fetch('https://jobs.github.com/positions.json?description=python&location=new+york', {
+        fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'GET',
+            mode: 'cors', cache: 'default',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' ,
+            },
+        }).then(result => result.json())
+        .then(items => this.setState({ items }));
+        /*
+         fetch('https://jobs.github.com/positions.json?description=python&location=new+york', { method: 'GET', headers: { 'Access-Control-Allow-Origin': '*' }, mode: 'cors', cache: 'default' })
+             .then(result => result.json())
+             .then(items => this.setState({ items }));
+             */
+        console.log(this.result);
+        console.log(this.items);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.result}
+
+                <ul>
+                    {this.state.items.map(item => <li>{item.name}</li>)}
+                </ul>
+            </div>
+        );
+    }
+}
+/*
+class ItemListerzxc extends Component {
+
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
+
+    componentDidMount() {
+        var component = this;
+        fetch("https://api.github.com/users/Raznagul", function (data) {
+            component.setState(data);
+            console.log(data);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={this.state.avatar_url} width="80" />
+                <h1>{this.state.name}</h1>
+            </div>
+        )
+    }
+}
+*/
+export default ItemLister;
 
 export { JobSearchFilter, JobSearchList, JobSearchContent };
