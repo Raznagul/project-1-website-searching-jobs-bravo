ef35455cda23697befe60b3df13fdb2231ff02a1
Raznagul <dachdavid@gmail.com>
2018-04-22 09:41:12 -0600

    tunning state handler, test example

--- searching-jobs-bravo/src/components/JobSearchPage.js
+++ searching-jobs-bravo/src/components/JobSearchPage.js
@@ -24,7 +24,7 @@ class JobSearchFilter extends Component {
         );
     }
 }
-
+/*
 class JobSearchList extends Component {
     render() {
         return (
@@ -36,7 +36,7 @@ class JobSearchList extends Component {
         );
     }
 }
-
+*/
 class JobSearchContent extends Component {
     render() {
         return (
@@ -98,7 +98,7 @@ class ItemListerasd extends Component {
     }
 }
 */
-class ItemLister extends Component {
+class JobSearchList extends Component {
     constructor() {
         super();
         this.state = { items: [] };
@@ -107,17 +107,17 @@ class ItemLister extends Component {
     }
 
     componentDidMount() {
-        //fetch('https://jobs.github.com/positions.json?description=python&location=new+york', {
-        fetch('https://jsonplaceholder.typicode.com/users', {
+        fetch('https://jobs.github.com/positions.json?description=python&location=new+york', {
+        //fetch('https://jsonplaceholder.typicode.com/users', {
             method: 'GET',
             mode: 'cors', cache: 'default',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*' ,
+                'Access-Control-Allow-Origin': '*',
             },
         }).then(result => result.json())
-        .then(items => this.setState({ items }));
+            .then(items => this.setState({ items }));
         /*
          fetch('https://jobs.github.com/positions.json?description=python&location=new+york', { method: 'GET', headers: { 'Access-Control-Allow-Origin': '*' }, mode: 'cors', cache: 'default' })
              .then(result => result.json())
@@ -130,11 +130,13 @@ class ItemLister extends Component {
     render() {
         return (
             <div>
-                {this.result}
-
-                <ul>
-                    {this.state.items.map(item => <li>{item.name}</li>)}
-                </ul>
+                {this.state.items.map(item =>
+                    <div>
+                        <p>{item.name}</p>
+                        <p>{item.username}</p>
+                        <p>{item.email}</p>
+                    </div>
+                )}
             </div>
         );
     }
@@ -165,6 +167,144 @@ class ItemListerzxc extends Component {
     }
 }
 */
-export default ItemLister;
+
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value
+});
+
+const UserForSignIn = {
+    qty: 0,
+};
+
+class Product extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { ...UserForSignIn };
+    }
+
+    buy = () => {
+        var component = this;
+        const { qty } = component.state;
+        //this.setState((state) => ({ qty: state.qty + 1 }));
+        this.setState(byPropKey("qty", component.state.qty + 1));
+    }
+
+    show = () => {
+        var component = this;
+        this.props.handleShow(component.props.name);
+        this.props.handletTotal(component.props.price);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.props.name} - {this.props.price}</p>
+                <button onClick={this.buy} >Buy</button>
+                <button onClick={this.show} >Show</button>
+                <h3>Qty: {this.state.qty} items</h3>
+                <hr />
+            </div>
+
+        );
+    }
+}
+
+class Total extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Total cash: {this.props.total} </h3>
+            </div>
+        );
+    }
+}
+
+class ProductForm extends Component {
+
+    submit(e) {
+        e.preventDefault();
+
+        //alert("name: " + this.refs.name.value + " - " + this.refs.price.value);
+
+        var product = {
+            name: this.refs.name.value,
+            price: parseInt(this.refs.price.value)
+        }
+
+        this.props.handleCreate(product);
+
+        this.refs.name.value = "";
+        this.refs.price.value = "";
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submit}>
+                <input type="text" placeholder="product name" ref="name" /> -
+                <input type="text" placeholder="product name" ref="name" />
+                <br />
+                <button  >Create product</button>
+                <hr />
+            </form>
+        );
+    }
+}
+
+class ProductList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            total: 0,
+            productList: [
+                { name: "android", price: 121 },
+                { name: "apple", price: 123 },
+                { name: "nokia", price: 65 }
+            ]
+        };
+    }
+
+    createProduct  = (product) => {
+        this.setState({
+            productList: this.state.productList.concat(product)
+        });
+    }
+
+    calculateTotal = (price) => {
+        var component = this;
+        this.setState({ total: component.state.total + price });
+        alert(this.state.total);
+
+    }
+
+    showProduct = name => {
+        alert("you selected " + name)
+    }
+
+    render() {
+        var component = this;
+        var products = this.state.productList.map(function (product) {
+            return (
+                <Product name={product.name} price={product.price}
+                    handleShow={component.showProduct}
+                    handletTotal={component.calculateTotal} />
+            )
+        });
+
+        return (
+            <div>
+                <ProductForm handleCreate={this.createProduct} />
+                {products}
+                <Total total={this.state.total} />
+            </div>
+        );
+    }
+}
+
+export default JobSearchList;
 
 export { JobSearchFilter, JobSearchList, JobSearchContent };
