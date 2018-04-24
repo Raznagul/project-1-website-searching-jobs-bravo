7694308650a5bde380e60c81b20f684c51ec0805
Raznagul <dachdavid@gmail.com>
2018-04-23 18:19:36 -0600

    added search component from landing page, removed test, change "full time" input from dropdown to checkbox

--- searching-jobs-bravo/src/components/JobSearchPage.js
+++ searching-jobs-bravo/src/components/JobSearchPage.js
@@ -8,6 +8,10 @@ import axios from 'axios';
 
 const uniq = a => [...new Set(a)];
 
+const findJobContent = (a, id) => {
+    return a.find(item => item.id === id);
+};
+
 class JobSearchFilter extends Component {
 
     constructor(props) {
@@ -23,11 +27,7 @@ class JobSearchFilter extends Component {
 
         return (
             <div>
-                <select name="Type">
-                    {this.props.type.map(type =>
-                        <option value={type}>{type}</option>)
-                    }
-                </select>
+                <label>Full time only</label><input type="checkbox" name="full_time" value="true" />
                 <input name="date" type="date" />
                 <select name="Company">
                     {this.props.location.map(location =>
@@ -100,9 +100,9 @@ class JobSearchList extends Component {
                         <p>{item.title}</p>
                         <p>{item.location}</p>
                         <p>{item.created_at}</p>
-                        <hr/>
+                        <hr />
                     </div>
-                    
+
                 )}
             </div>
         );
@@ -112,7 +112,7 @@ class JobSearchList extends Component {
 class JobSearchPage extends Component {
     constructor(props) {
         super(props);
-        this.state = { items: [] };
+        this.state = { job: "", items: [] };
 
     }
 
@@ -128,13 +128,20 @@ class JobSearchPage extends Component {
     }
 
     render() {
-        console.log(this.state.items.find(item => item.id === "40af75b0-43da-11e8-8421-06fdf93a68d5"));
+        console.log(findJobContent(this.state.items, "40af75b0-43da-11e8-8421-06fdf93a68d5"));
+
         return (
 
             <div className="container ">
+
                 <div className="row">
                     <div className="col-sm ">
-                        <JobSearchFilter type={uniq(this.state.items.map(item => item.type))} location={uniq(this.state.items.map(item => item.location))} />
+                        <LocationSearchInput />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm ">
+                        <JobSearchFilter location={uniq(this.state.items.map(item => item.location))} />
                     </div>
                 </div>
                 <div className="row">
@@ -142,158 +149,9 @@ class JobSearchPage extends Component {
                         <JobSearchList jobList={this.state.items.map(({ title, location, created_at }) => ({ ["title"]: title, ["location"]: location, ["created_at"]: created_at }))} />
                     </div>
                     <div className="col-sm-9">
-                        <JobSearchContent jobContent={this.state.items.find(item => item.id === "40af75b0-43da-11e8-8421-06fdf93a68d5")} />
+                        <JobSearchContent jobContent={findJobContent(this.state.items, "40af75b0-43da-11e8-8421-06fdf93a68d5")} />
                     </div>
                 </div>
-            </div>
-        );
-    }
-}
-
-/*
-class ItemListerzxc extends Component {
-
-    constructor() {
-        super();
-        this.state = { items: [] };
-    }
-
-    componentDidMount() {
-        var component = this;
-        fetch("https://api.github.com/users/Raznagul", function (data) {
-            component.setState(data);
-            console.log(data);
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <img src={this.state.avatar_url} width="80" />
-                <h1>{this.state.name}</h1>
-            </div>
-        )
-    }
-}
-*/
-
-class Product extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { qty: 0 };
-    }
-
-    buy = () => {
-        this.setState((state) => ({ qty: state.qty + 1 }));
-        //this.setState(qty: this.state.qty + 1);
-        this.props.handletTotal(this.props.price);
-    }
-
-    show = () => {
-        this.props.handleShow(this.props.name);
-    }
-
-    render() {
-        return (
-            <div>
-                <p>{this.props.name} - {this.props.price}</p>
-                <button onClick={this.buy} >Buy</button>
-                <button onClick={this.show} >Show</button>
-                <h3>Qty: {this.state.qty} items</h3>
-                <hr />
-            </div>
-
-        );
-    }
-}
-
-class Total extends Component {
-
-    render() {
-        return (
-            <div>
-                <h3>Total cash: {this.props.total} </h3>
-            </div>
-        );
-    }
-}
-
-class ProductForm extends Component {
-
-    submit = (e) => {
-        e.preventDefault();
-
-        var product = {
-            name: this.refs.name.value,
-            price: parseInt(this.refs.price.value)
-        }
-
-        this.props.handleCreate(product);
-
-        this.refs.name.value = "";
-        this.refs.price.value = "";
-
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.submit}>
-                <input type="text" placeholder="product name" ref="name" /> -
-                <input type="text" placeholder="product price" ref="price" />
-                <br />
-                <button  >Create product</button>
-                <hr />
-            </form>
-        );
-    }
-}
-
-class ProductList extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            total: 0,
-            productList: [
-                { name: "android", price: 121 },
-                { name: "apple", price: 123 },
-                { name: "nokia", price: 65 }
-            ]
-        };
-    }
-
-    createProduct = (product) => {
-        this.setState({
-            productList: this.state.productList.concat(product)
-        });
-    }
-
-    calculateTotal = (price) => {
-        this.setState({ total: this.state.total + price });
-
-    }
-
-    showProduct = name => {
-        alert("you selected " + name)
-    }
-
-    render() {
-        var component = this;
-        var products = this.state.productList.map(function (product) {
-            return (
-                <Product name={product.name} price={product.price}
-                    handleShow={component.showProduct}
-                    handletTotal={component.calculateTotal} />
-            )
-        });
-
-        return (
-            <div>
-                <ProductForm handleCreate={this.createProduct} />
-                {products}
-                <Total total={this.state.total} />
             </div>
         );
     }
